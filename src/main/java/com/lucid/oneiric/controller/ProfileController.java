3feb3b702049c05eaa428ada7ec61b10568905bb
Samuel <samuelbarbosa.ipms@gmail.com>
2024-02-14 05:40:31 -0300

    feat: 2 new endpoints added (/profile and /isLoggedIn)

---
+++ src/main/java/com/lucid/oneiric/controller/ProfileController.java
@@ -0,0 +1,31 @@
+package com.lucid.oneiric.controller;
+
+import com.lucid.oneiric.security.UserPrincipal;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public ResponseEntity<Object> getUserProfileInformation() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        String username = userPrincipal.getUsername();
+        return ResponseEntity.ok().body(new Object() {
+            public final String username = userPrincipal.getUsername();
+            public final String email = userPrincipal.getEmail();
+
+        });
+    }
+
+    @GetMapping("/isLoggedIn")
+    public ResponseEntity<String> getUserAuthenticationStatus() {
+            return ResponseEntity.ok().body("User authenticated.");
+        }
+
+}

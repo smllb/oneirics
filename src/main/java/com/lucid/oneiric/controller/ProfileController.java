40b9c29dca183fa0e6f250e92294d9527c5e505b
Samuel <samuelbarbosa.ipms@gmail.com>
2024-02-24 03:17:04 -0300

    feat: added proper isLoggedIn endpoint

--- src/main/java/com/lucid/oneiric/controller/ProfileController.java
+++ src/main/java/com/lucid/oneiric/controller/ProfileController.java
@@ -1,11 +1,16 @@
 package com.lucid.oneiric.controller;
 
 import com.lucid.oneiric.security.UserPrincipal;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ProfileController {
@@ -23,8 +28,15 @@ public class ProfileController {
     }
 
     @GetMapping("/isLoggedIn")
-    public ResponseEntity<String> getUserAuthenticationStatus() {
-            return ResponseEntity.ok().body("User authenticated.");
+    public ResponseEntity<Map<String, String>> getUserAuthenticationStatus() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Map<String, String> response = new HashMap<>();
+        if (authentication != null && !(authentication instanceof AnonymousAuthenticationToken)) {
+            response.put("message", "User authenticated.");
+            return ResponseEntity.ok(response);
+        } else {
+            response.put("message", "User not authenticated.");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
-
+    }
 }

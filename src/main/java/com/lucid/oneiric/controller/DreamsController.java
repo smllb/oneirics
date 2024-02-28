514361f4441724098def1307b4cfb597cf368f60
Samuel <samuelbarbosa.ipms@gmail.com>
2024-02-28 05:57:22 -0300

    feat: rework of existing endpoints, added new endpoints ("/dreams/search/public", "dreams/remove/{dreamId}"). better user control, new functions on service layer. added mapper to handle dreamDTOs

--- src/main/java/com/lucid/oneiric/controller/DreamsController.java
+++ src/main/java/com/lucid/oneiric/controller/DreamsController.java
@@ -3,64 +3,107 @@ package com.lucid.oneiric.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.lucid.oneiric.dto.DreamDTO;
 import com.lucid.oneiric.dto.UserNewDreamDTO;
+import com.lucid.oneiric.entities.DreamEntity;
+import com.lucid.oneiric.entities.UserEntity;
 import com.lucid.oneiric.services.DreamService;
+import com.lucid.oneiric.services.UsersService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class DreamsController {
 
     public DreamService dreamService;
+    private final UsersService usersService;
 
-    public DreamsController(DreamService dreamService) {
+    public DreamsController(DreamService dreamService, UsersService usersService) {
         this.dreamService = dreamService;
+        this.usersService = usersService;
     }
 
-    @PostMapping("/newdream")
+    @PostMapping("/dream/create")
     @ResponseStatus(code = HttpStatus.CREATED)
     public ResponseEntity<String> newDream(@RequestBody UserNewDreamDTO userNewDreamDTO) {
         System.out.println(userNewDreamDTO.toString());
         return dreamService.saveNewDream((userNewDreamDTO));
     }
 
-    @GetMapping("/dream/{id}")
+    @GetMapping("/dream/search/{id}")
     public ResponseEntity<DreamDTO> getDreamByDreamId(@PathVariable String id) {
         System.out.println(id);
-        DreamDTO dream = dreamService.getDreamById(id);
-        return ResponseEntity.ok(dream);
+        DreamEntity dream = dreamService.getDreamEntityById(id);
+        if (dream != null) {
+            if (dream.getVisibilityEntity().getId().equals(1)) {
+                if (SecurityContextHolder.getContext().getAuthentication().getName().equals(dream.getUserEntity().getLogin())) {
+                    return ResponseEntity.ok(dreamService.toDTO(dream));
+                }
+                return ResponseEntity.status(403).build();
+            } else {
+                return ResponseEntity.ok(dreamService.toDTO(dream));
+            }
+        }
+
+        return ResponseEntity.status(400).build();
+    }
+
+    @GetMapping("/dreams/search/public")
+    public ResponseEntity<List<DreamDTO>> getAllPublicDreams() {
+        return ResponseEntity.ok(dreamService.getDreamsDTOByVisibilityId(2));
 
     }
 
-    @GetMapping("/dreams/visibility/{visibility}")
-    public ResponseEntity<List<DreamDTO>> getAllDreamsByVisibilityId(@PathVariable String visibility) throws JsonProcessingException {
-        System.out.println("dream by visibility");
-        List<DreamDTO> dreams = dreamService.getDreamsByVisibilityId(visibility);
-        return ResponseEntity.ok(dreams);
+
+    // only for adm
+    @GetMapping("/dreams/search/visibility/{visibilityId}")
+    public ResponseEntity<List<DreamDTO>> getAllDreamsByVisibilityId(@PathVariable Integer visibilityId) throws JsonProcessingException {
+        return ResponseEntity.ok(dreamService.getDreamsDTOByVisibilityId(visibilityId));
 
     }
-//
-    @GetMapping("dreams/session/")
+
+    @GetMapping("dreams/search/session/")
     public ResponseEntity<List<DreamDTO>> getAllDreamsByUser() {
-//        System.out.println("dreams by user");
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         List<DreamDTO> userDreams = dreamService.getDreamsByUser(username);
         return ResponseEntity.ok(userDreams);
 
     }
 
-    @GetMapping("dreams/user/{username}")
+    @GetMapping("dreams/search/user/{username}")
     public ResponseEntity<List<DreamDTO>> getAllDreamsByUser(@PathVariable String username) {
-//        System.out.println("dreams by user");
-        // ADD USER CONTROL HERE!!!
-        List<DreamDTO> userDreams = dreamService.getDreamsByUser(username);
+        List<DreamDTO> userDreams = new ArrayList<>();
+
+        if (SecurityContextHolder.getContext().getAuthentication().getName().equals(username)) {
+            userDreams = dreamService.getDreamsByUser(username);
+        } else {
+            userDreams = dreamService.getAllDreamsDTOByUserEntityLoginAndVisibilityEntityId(username, 2);
+        }
         return ResponseEntity.ok(userDreams);
 
+    }
+
+    @DeleteMapping("dreams/remove/{dreamId}")
+    public ResponseEntity<String> removeDreamById(@PathVariable String dreamId) {
+        UserEntity user = usersService.getUserByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
+        DreamEntity dreamToBeRemoved = dreamService.getDreamEntityById(dreamId);
+        if (dreamToBeRemoved == null) {
+            return ResponseEntity.status(400).body("No dream with that id.");
+
+        } else {
+            if (user.getLogin().equals(dreamToBeRemoved.getUserEntity().getLogin()) || user.isAdmin()) {
+                if (dreamService.removeDreamById(dreamId)) {
+                    return ResponseEntity.ok("Removed dream with id " + dreamId);
+
+                }
+                return ResponseEntity.status(500).body("Couldn't delete dream. ");
+
+            }
+        }
+        return ResponseEntity.status(403).body("Insufficient rights.");
     }
 
 }
--- src/main/java/com/lucid/oneiric/services/DreamService.java
+++ src/main/java/com/lucid/oneiric/services/DreamService.java
@@ -3,28 +3,35 @@ package com.lucid.oneiric.services;
 import com.lucid.oneiric.dto.DreamDTO;
 import com.lucid.oneiric.dto.UserNewDreamDTO;
 import com.lucid.oneiric.entities.*;
+import com.lucid.oneiric.mappers.DreamMapper;
 import com.lucid.oneiric.repository.*;
 import com.lucid.oneiric.security.UserPrincipal;
+import jakarta.transaction.Transactional;
+import lombok.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DreamService {
 
     private final DreamRepository dreamRepository;
+    private final DreamMapper dreamMapper;
     private final UsersRepository userRepository;
     private final DreamTypeRepository dreamTypeRepository;
     private final DreamCategoryRepository dreamCategoryRepository;
     private final VisibilityRepository visibilityRepository;
 
-    public DreamService(DreamRepository dreamRepository, UsersRepository userRepository, DreamTypeRepository dreamTypeRepository, DreamCategoryRepository dreamCategoryRepository, VisibilityRepository visibilityRepository) {
+    public DreamService(DreamRepository dreamRepository, DreamMapper dreamMapper, UsersRepository userRepository, DreamTypeRepository dreamTypeRepository, DreamCategoryRepository dreamCategoryRepository, VisibilityRepository visibilityRepository) {
         this.dreamRepository = dreamRepository;
+        this.dreamMapper = new DreamMapper();
         this.userRepository = userRepository;
         this.dreamTypeRepository = dreamTypeRepository;
         this.dreamCategoryRepository = dreamCategoryRepository;
@@ -32,6 +39,7 @@ public class DreamService {
     }
 
 
+    @Transactional
     public ResponseEntity<String> saveNewDream(UserNewDreamDTO userNewDreamDTO) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
@@ -45,23 +53,45 @@ public class DreamService {
         DreamEntity newDream = new DreamEntity(author, dreamKind, dreamCategory, visibility, userNewDreamDTO.getDreamTitle(), userNewDreamDTO.getDreamContent());
         try {
             dreamRepository.save(newDream);
+            if (dreamKind.getId().equals(1)) {
+                author.setRegularDreamCount(author.getRegularDreamCount()+1);
+            } else {
+                author.setLucidDreamCount(author.getRegularDreamCount()+1);
+            }
             return ResponseEntity.status(HttpStatus.OK).body(newDream.getId());
+
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad bad bad");
+
         }
     }
 
-    public DreamDTO getDreamById(String dreamId) {
-        return dreamRepository.fillDreamDTOByDreamId(dreamId);
+    public DreamDTO getDreamDTOById(String dreamId) {
+        try {
+            @NonNull Optional<DreamEntity> dream = dreamRepository.findById(dreamId);
+
+            return dreamMapper.dreamEntityToDto(dream);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+
+        }
 
     }
-    
-    public List<DreamDTO>  getDreamsByVisibilityId(String visibilityId) {
+
+    public List<DreamDTO> getDreamsDTOByVisibilityId(Integer visibilityId) {
         try {
-            List<DreamDTO> dreams = dreamRepository.fillDreamListByVisibility(visibilityId);
-            return dreams;
-//            return new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(dreams);
-            
+            List<DreamDTO> dreamsDTO = new ArrayList<>();
+            Optional<List<DreamEntity>> optionalDreams = Optional.ofNullable(dreamRepository.findAllByVisibilityEntityId(visibilityId));
+
+            if (optionalDreams.isPresent()) {
+                for (DreamEntity dream : optionalDreams.get()) {
+                    dreamsDTO.add(dreamMapper.dreamEntityToDto(Optional.ofNullable(dream)));
+                }
+                return dreamsDTO;
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
 
@@ -70,19 +100,65 @@ public class DreamService {
 
     }
 
+    public List<DreamDTO> getAllDreamsDTOByUserEntityLoginAndVisibilityEntityId(String userEntity_login, Integer visibilityEntity_id) {
+        List<DreamDTO> dreamsDTO = new ArrayList<>();
+        Optional<List<DreamEntity>> optionalDreams = dreamRepository.findAllByUserEntityLoginAndVisibilityEntityId(userEntity_login, visibilityEntity_id);
+        if (optionalDreams.isPresent()) {
+            for (DreamEntity dream : optionalDreams.get()) {
+                dreamsDTO.add(dreamMapper.dreamEntityToDto(Optional.ofNullable(dream)));
+            }
+            return dreamsDTO;
+        }
+        return null;
+
+    }
+    public List<DreamEntity> getAllDreamEntitiesByVisibilityId(Integer visibilityId) {
+        return dreamRepository.findAllByVisibilityEntityId(visibilityId);
+
+    }
+    public List<DreamEntity> getAllByUserEntityLogin(String entityLogin) {
+        return dreamRepository.findAllByUserEntityLogin(entityLogin);
+
+    }
 
     public List<DreamDTO> getDreamsByUser(String username) {
         try {
-            List<DreamDTO> dreams = dreamRepository.fillDreamListByUsername(username);
-            return dreams;
-//            return new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(dreams);
+            List<DreamDTO> dreamsDTO = new ArrayList<>();
+            List<DreamEntity> dreams = dreamRepository.findAllByUserEntityLogin(username);
+            for (DreamEntity dream : dreams) {
+                dreamsDTO.add(dreamMapper.dreamEntityToDto(Optional.ofNullable(dream)));
+            }
+            return dreamsDTO;
 
         } catch (Exception e) {
             e.printStackTrace();
 
-
         }
         return Collections.emptyList();
+
+    }
+
+    @Transactional
+    public Boolean removeDreamById(String id) {
+        Optional<DreamEntity> optionalDream = dreamRepository.findById(id);
+        if (optionalDream.isPresent()) {
+            dreamRepository.delete(optionalDream.get());
+            return true;
+        }
+        return false;
+    }
+
+    public DreamEntity getDreamEntityById(String id) {
+        Optional<DreamEntity> optionalDream = dreamRepository.findById(id);
+        if (optionalDream.isPresent()) {
+            return optionalDream.get();
+        }
+        System.out.println("no dream with that id");
+        return null;
+    }
+
+    public DreamDTO toDTO (DreamEntity dream) {
+        return dreamMapper.dreamEntityToDto(Optional.ofNullable(dream));
 
     }
 

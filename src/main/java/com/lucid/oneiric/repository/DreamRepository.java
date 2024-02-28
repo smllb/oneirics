6512e2092065e244cf7b17de063f95ebdf050a6f
Samuel <samuelbarbosa.ipms@gmail.com>
2024-02-28 05:52:51 -0300

    chore: dreamRepository now sending back only dreamEntities or lists containing dreamEntities.

--- src/main/java/com/lucid/oneiric/repository/DreamRepository.java
+++ src/main/java/com/lucid/oneiric/repository/DreamRepository.java
@@ -15,45 +15,30 @@ import java.util.Optional;
 public interface DreamRepository extends JpaRepository<DreamEntity, String> {
     @NonNull
     @Override
-    public Optional<DreamEntity> findById(@NonNull  String id);
+    Optional<DreamEntity> findById(@NonNull  String id);
 
-//    public List<DreamEntity> findAllByAuthorId(@NonNull String id);
 
-//    public List<DreamEntity> findAllByAuthorIdAndCreationDate(@NonNull String authorId, @NonNull LocalDate creationDate);
-//
-//    public List<DreamEntity> findAllByAuthorIdAndVisibilityId(@NonNull String authorId, @NonNull Integer visibilityId);
-//
-//    public List<DreamEntity> findAllByVisibilityId(@NonNull Integer visibilityId);
+     List<DreamEntity> findAllByDreamTitleLikeIgnoreCase(String dreamTitle);
 
-    public List<DreamEntity> findAllByDreamTitleLikeIgnoreCase(@NonNull String dreamTitle);
+     DreamEntity findByVisibilityEntityId(Integer visibilityId);
 
-    @Query("SELECT new com.lucid.oneiric.dto.DreamDTO(d.id, d.creationDate, u.login, dt.kind, dc.category, v.name, d.dreamContent, d.dreamTitle) " +
-            "FROM DreamEntity d " +
-            "INNER JOIN d.userEntity u " +
-            "INNER JOIN d.dreamKindEntity dt " +
-            "INNER JOIN d.dreamCategoryEntity dc " +
-            "INNER JOIN d.visibilityEntity v " +
-            "WHERE d.id = :id")
-    DreamDTO fillDreamDTOByDreamId(@Param("id") String id);
+     List<DreamEntity> findAllByVisibilityEntityId(Integer visibilityId);
 
-    @Query("SELECT new com.lucid.oneiric.dto.DreamDTO(d.id, d.creationDate, u.login, dt.kind, dc.category, v.name, d.dreamContent, d.dreamTitle) " +
-            "FROM DreamEntity d " +
-            "INNER JOIN d.userEntity u " +
-            "INNER JOIN d.dreamKindEntity dt " +
-            "INNER JOIN d.dreamCategoryEntity dc " +
-            "INNER JOIN d.visibilityEntity v " +
-            "WHERE v.id = :id")
-    List<DreamDTO> fillDreamListByVisibility(@Param("id") String id);
+     Optional<List<DreamEntity>> findAllByUserEntityLoginAndVisibilityEntityId(String userEntity_login, Integer visibilityEntity_id);
 
-    @Query("SELECT new com.lucid.oneiric.dto.DreamDTO(d.id, d.creationDate, u.login, dt.kind, dc.category, v.name, d.dreamContent, d.dreamTitle) " +
-            "FROM DreamEntity d " +
-            "INNER JOIN d.userEntity u " +
-            "INNER JOIN d.dreamKindEntity dt " +
-            "INNER JOIN d.dreamCategoryEntity dc " +
-            "INNER JOIN d.visibilityEntity v " +
-            "WHERE u.login = :username "
-    )
-    List<DreamDTO> fillDreamListByUsername(@Param("username") String username);
+     List<DreamEntity> findAllByUserEntityLogin(String userEntity_login);
+
+     List<DreamEntity> findAllByDreamCategoryEntityId(Integer dreamCategoryEntity_id);
+     void deleteById(@NonNull String id);
+
+     void deleteAllByUserEntityLogin(@NonNull String userEntity_login);
+
+
+
+
+
+
+
 
     // i'll see what i do later.if i don't get a better idea, i'll just fill DreamEntities and convert them into the needed DTO but i feel like filling the all entities inside a dreamentity all the time is not very performative
 
